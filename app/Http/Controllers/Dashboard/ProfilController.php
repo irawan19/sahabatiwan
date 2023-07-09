a934ebb94e0ab67e9a82467370f4e39bea926aa6
Irawan Agung Nugroho <irawan.nugroho@spesolution.com>
2023-07-09 17:06:27 +0700

    update profil

--- app/Http/Controllers/Dashboard/ProfilController.php
+++ app/Http/Controllers/Dashboard/ProfilController.php
@@ -18,7 +18,104 @@ class ProfilController extends AdminCoreController
 
     public function prosesedit(Request $request)
     {
+        $link_profil = 'profil';
+        if (General::hakAkses($link_profil, 'lihat') == 'true')
+        {
+            $aturan = [
+                'text1_profils'                 => 'required',
+                'text2_profils'                 => 'required',
+                'nama_profils'                  => 'required',
+                'keterangan_nama_profils'       => 'required',
+                'konten_profils'                => 'required',
+                'url_youtube_profils'           => 'required',
+            ];
+            $error_pesan = [
+                'text1_profils.required'            => 'Form Text 1 Harus Diisi.',
+                'text2_profils.required'            => 'Form Text 2 Harus Diisi.',
+                'nama_profils.required'             => 'Form Nama Harus Diisi.',
+                'keterangan_nama_profils.required'  => 'Form Keterangan Harus Diisi.',
+                'konten_profils.required'           => 'Form Konten Harus Diisi.',
+                'url_youtube_profils.required'      => 'Form URL Youtube Harus Diisi.',
+            ];
+            $this->validate($request, $aturan, $error_pesan);
 
+            $profils_data = [
+                'text1_profils'                     => $request->text1_profils,
+                'text2_profils'                     => $request->text2_profils,
+                'nama_profils'                      => $request->nama_profils,
+                'keterangan_nama_profils'           => $request->keterangan_nama_profils,
+                'konten_profils'                    => $request->konten_profils,
+                'url_youtube_profils'               => $request->url_youtube_profils,
+                'updated_at'                        => date('Y-m-d H:i:s'),
+            ];
+            Master_profil::query()->update($profils_data);
+
+            if(!empty($request->userfile_foto1))
+            {
+                $aturan = [
+                    'userfile_foto1'     => 'required|mimes:png,jpg,jpeg,svg',
+                ];
+                $error_pesan = [
+                    'userfile_foto1.required'   => 'Form Foto 1 Harus Diisi.',
+                ];
+                $this->validate($request, $aturan, $error_pesan);
+    
+                $cek_foto1       = Master_profil::first();
+                if (!empty($cek_foto1)) {
+                    $foto1_lama        = $cek_foto1->foto1_profils;
+                    if (Storage::disk('public')->exists($foto1_lama))
+                        Storage::disk('public')->delete($foto1_lama);
+                }
+    
+                $nama_foto1 = date('Ymd') . date('His') . str_replace(')', '', str_replace('(', '', str_replace(' ', '-', $request->file('userfile_foto1')->getClientOriginalName())));
+                $path_foto1 = 'profil/';
+                Storage::disk('public')->put($path_foto1.$nama_foto1, file_get_contents($request->file('userfile_foto1')));
+    
+                $data = [
+                    'foto1_profils'     => $path_foto1 . $nama_foto1,
+                    'updated_at'        => date('Y-m-d H:i:s'),
+                ];
+    
+                Master_profil::query()->update($data);
+            }
+
+            if(!empty($request->userfile_foto2))
+            {
+                $aturan = [
+                    'userfile_foto2'     => 'required|mimes:png,jpg,jpeg,svg',
+                ];
+                $error_pesan = [
+                    'userfile_foto2.required'   => 'Form Foto 2 Harus Diisi.',
+                ];
+                $this->validate($request, $aturan, $error_pesan);
+    
+                $cek_foto2       = Master_profil::first();
+                if (!empty($cek_foto2)) {
+                    $foto2_lama        = $cek_foto2->foto2_profils;
+                    if (Storage::disk('public')->exists($foto2_lama))
+                        Storage::disk('public')->delete($foto2_lama);
+                }
+    
+                $nama_foto2 = date('Ymd') . date('His') . str_replace(')', '', str_replace('(', '', str_replace(' ', '-', $request->file('userfile_foto2')->getClientOriginalName())));
+                $path_foto2 = 'profil/';
+                Storage::disk('public')->put($path_foto2.$nama_foto2, file_get_contents($request->file('userfile_foto2')));
+    
+                $data = [
+                    'foto2_profils'     => $path_foto2 . $nama_foto2,
+                    'updated_at'        => date('Y-m-d H:i:s'),
+                ];
+    
+                Master_profil::query()->update($data);
+            }
+
+            $setelah_simpan = [
+                'alert'                     => 'sukses',
+                'text'                      => 'Data berhasil diperbarui',
+            ];
+            return redirect()->back()->with('setelah_simpan', $setelah_simpan)->withInput($request->all());
+        }
+        else
+            return redirect('dashboard');
     }
 
 }
--- resources/views/dashboard/profil/lihat.blade.php
+++ resources/views/dashboard/profil/lihat.blade.php
@@ -7,7 +7,7 @@
 				<form class="form-horizontal m-t-40" action="{{ URL('dashboard/profil/prosesedit') }}" enctype="multipart/form-data" method="POST">
 					{{ csrf_field() }}
 					<div class="card-header">
-						<strong>Konfigurasi Aplikasi</strong>
+						<strong>Profil</strong>
 					</div>
 					<div class="card-body">
 						@if (Session::get('setelah_simpan.alert') == 'sukses')
@@ -59,6 +59,11 @@
 							<label class="form-col-form-label" for="konten_profils">Konten <b style="color:red">*</b></label>
 							<textarea class="form-control {{ General::validForm($errors->first('konten_profils')) }}" id="konten_profils" name="konten_profils" rows="5">{{Request::old('konten_profils') == '' ? $lihat_profils->konten_profils : Request::old('konten_profils')}}</textarea>
 							{{General::pesanErrorForm($errors->first('konten_profils'))}}
+						</div>
+						<div class="form-group">
+							<label class="form-col-form-label" for="url_youtube_profils">URL Youtube <b style="color:red">*</b></label>
+							<input class="form-control {{ General::validForm($errors->first('url_youtube_profils')) }}" id="url_youtube_profils" type="text" name="url_youtube_profils" value="{{Request::old('url_youtube_profils') == '' ? $lihat_profils->url_youtube_profils : Request::old('url_youtube_profils')}}">
+							{{General::pesanErrorForm($errors->first('url_youtube_profils'))}}
 						</div>
 					</div>
 	                <div class="card-footer right-align">
