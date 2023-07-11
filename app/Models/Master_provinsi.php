d24d1efb77a12412fe89de7150fe5c64d443ba7c
Irawan Agung Nugroho <irawan.nugroho@spesolution.com>
2023-07-11 22:31:32 +0700

    update swara nusvantara

--- app/Models/Master_provinsi.php
+++ app/Models/Master_provinsi.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Master_provinsi extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 }
---
+++ database/migrations/2023_07_11_191427_create_master_swara_nusvantaras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('master_swara_nusvantaras', function (Blueprint $table) {
+            $table->id('id_swara_nusvantaras');
+            $table->bigInteger('kategori_swara_nusvantaras_id')->unsigned()->index()->nullable();
+            $table->foreign('kategori_swara_nusvantaras_id')->references('id_kategori_swara_nusvantaras')->on('master_kategori_swara_nusvantaras')->onUpdate('set null')->onDelete('set null');
+            $table->string('foto_swara_nusvantaras');
+            $table->string('judul_swara_nusvantaras');
+            $table->longtext('konten_swara_nusvantaras');
+            $table->datetime('tanggal_publikasi_swara_nusvantaras');
+            $table->double('total_baca_swara_nusvantaras');
+            $table->double('total_komentas_swara_nusvantaras');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('master_swara_nusvantaras');
+    }
+};
