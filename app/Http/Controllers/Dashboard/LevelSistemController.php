0f2c91d45ca1ecb99ffa0677586ab9ca24276d50
= <irawan_agungnugroho@yahoo.com>
2023-07-23 17:06:09 +0700

    update

--- app/Http/Controllers/Dashboard/LevelSistemController.php
+++ app/Http/Controllers/Dashboard/LevelSistemController.php
@@ -216,12 +216,6 @@ class LevelSistemController extends AdminCoreController
             {
                 if($id_level_sistems != 1)
                 {
-                    $users_data = [
-                        'level_sistems_id'  => 1
-                    ];
-                    User::where('level_sistems_id',$id_level_sistems)
-                                    ->update($users_data);
-
                     DB::raw("DELETE t1.*, t2.* FROM master_level_sistems t1 LEFT JOIN master_akses t2 ON t2.level_sistems_id=t1.id_level_sistems WHERE id_level_sistems='$id_level_sistems'");
 
                     return response()->json(["sukses" => "sukses"], 200);
