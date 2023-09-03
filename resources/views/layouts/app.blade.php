ba6206c4d6009199a25157304906125025cb61f9
= <irawan_agungnugroho@yahoo.com>
2023-09-03 16:01:16 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Security-Policy" 
+    content="
+      worker-src blob:; 
+      child-src blob: gap:;
+      img-src 'self' blob: data:;
+      default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content:">
     <title>{{$lihat_konfigurasi_aplikasis->nama_konfigurasi_aplikasis}}</title>
     <link rel="apple-touch-icon" sizes="180x180" href="{{URL::asset('storage/'.$lihat_konfigurasi_aplikasis->icon_konfigurasi_aplikasis)}}" />
     <link rel="icon" type="image/png" sizes="32x32" href="{{URL::asset('storage/'.$lihat_konfigurasi_aplikasis->icon_konfigurasi_aplikasis)}}" />
