dd688f8d1e347c4deb319291e164fed0ef9ba9b2
= <irawan_agungnugroho@yahoo.com>
2023-10-08 09:51:40 +0700

    update mobile

--- resources/views/mobile/layouts/footer.blade.php
+++ resources/views/mobile/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 @php($lihat_profils = \App\Models\Master_profil::first())
-<footer class="site-footer" style="margin:0 auto; text-align:center">
+<footer class="site-footer">
     <div class="site-footer__img">
         <img src="{{URL::asset('template/front/images/resources/site-footer-img.jpg')}}" alt="{{$lihat_konfigurasi_aplikasis->nama_konfigurasi_aplikasis}}">
     </div>
@@ -13,7 +13,7 @@
         </div>
         <div class="site-footer__middle">
             <div class="row">
-                <div class="col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="100ms">
+                <div class="col-xl-8 col-lg-8 col-md-6 wow fadeInUp" data-wow-delay="100ms" style="text-align:left">
                     <div class="footer-widget__column footer-widget__Contact">
                         <div class="footer-widget__title-box">
                             <h3 class="footer-widget__title">{{$lihat_profils->nama_profils}}</h3>
@@ -46,7 +46,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xl-6 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="200ms">
+                <div class="col-xl-4 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="200ms" style="text-align:left">
                     <div class="footer-widget__column footer-widget__link">
                         <div class="footer-widget__title-box">
                             <h3 class="footer-widget__title">Link</h3>
